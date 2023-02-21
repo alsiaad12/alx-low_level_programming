5e756b45871aeb4fbe7fbed1d8568ff7ebe72c49
alsiaad12 <ms4439409@gmail.com>
2023-02-21 09:45:26 -0800

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
-/**
- *  * main - Print "_putchar" followed by a new line.
- *   * Description: You are not allowed to include standard libraries.
- *    * Return: 0
- *     */
-int main(void)
-{
-		char holberton[] = "_putchar";
-			int i = 0;
+	/**
+	 * 	 * main - print _putchar
+	 * 	 	 * is written by mbah
+	 * 	 	 	 * Return: 0
+	 * 	 	 	 	 */
 
-				while (holberton[i] != '\0')
-						{
-									_putchar(holberton[i]);
-											i++;
-												}
-					_putchar('\n');
 
-						return (0);
-}
+	int main(void)
+		{
+				_putchar('_');
+					_putchar('p');
+						_putchar('u');
+							_putchar('t');
+								_putchar('c');
+									_putchar('h');
+										_putchar('a');
+											_putchar('r');
+												_putchar('\n');
+													return (0);
+														}
+
